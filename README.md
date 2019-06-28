d070e41cfab640c0a8cfb9c23ac18e60fce38464
cgrossi <cgrossi@gmail.com>
2019-06-27 23:26:40 -0600

    Add new task functionality and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Task List App With Vanilla Javascript
+
+Task list app made from scratch using materialize for icons.
+
+## Features Not Yet Implemented
+
+- Filter tasks
+- Search tasks
+- Connect to local storage
+
+## Bugs
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function clickCheckbox() {
-        const checkboxes = document.querySelectorAll('#completed');
+        const checkboxes = document.querySelectorAll('.checkbox');
         checkboxes.forEach(el => {
                 el.addEventListener('click', event => {
                         if (event.target.checked) {
@@ -33,7 +33,7 @@ function editTodo() {
         const editModalSubmit = document.querySelector('.edit-modal button');
         let current;
         todoButtons.forEach((el, i) => {
-                el.addEventListener('click', event => {
+                el.addEventListener('click', () => {
                         editModal.style.display = 'block';
                         current = i;
                 });
@@ -46,10 +46,50 @@ function editTodo() {
         });
 }
 
+function addTodo() {
+        const addTask = document.querySelector('.add-task-btn');
+        const addModal = document.querySelector('.add-modal-container');
+        const addSubmit = document.querySelector('.add-modal .save');
+        const addInput = document.querySelector('.add-modal-input');
+        const tasks = document.querySelector('.tasks');
+        addTask.addEventListener('click', () => {
+                addModal.style.display = 'block';
+                addInput.value = '';
+        });
+        addSubmit.addEventListener('click', event => {
+                const li = document.createElement('li');
+                const input = document.createElement('input');
+                const p = document.createElement('p');
+                const div = document.createElement('div');
+                li.classList.add('task');
+                input.setAttribute('type', 'checkbox');
+                input.setAttribute('class', 'checkbox');
+                p.textContent = addInput.value;
+                div.setAttribute('class', 'modify');
+                const i = document.createElement('i');
+                const idelete = document.createElement('i');
+                i.setAttribute('class', 'small material-icons edit');
+                i.textContent = 'edit';
+                idelete.setAttribute('class', 'small material-icons delete');
+                idelete.textContent = 'delete';
+                li.appendChild(input);
+                li.appendChild(p);
+                div.appendChild(i);
+                div.appendChild(idelete);
+                li.appendChild(div);
+                tasks.appendChild(li);
+                clickCheckbox();
+                deleteTodo();
+                editTodo();
+                addModal.style.display = 'none';
+        });
+}
+
 function main() {
         clickCheckbox();
         deleteTodo();
         editTodo();
+        addTodo();
 }
 
 main();
